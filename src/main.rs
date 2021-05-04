ed4df03b79bb145d5f101e9f408d78f651d66f97
AshF0x <peer.heinen@gmail.com>
2021-05-04 13:07:32 +0200

    added timestamp and user-agent feature

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::App;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use isahc::prelude::*;
+use http::Request;
+use chrono;
 
 fn main() {
     let args = App::new("dirruster")
@@ -10,15 +12,28 @@ fn main() {
        .author("written by AshF0x")
        .args_from_usage("
        -u, --url=[TARGET_URL] 'Sets your target URL(required)'
-       -w, --wordlist=[PATH_TO_WORDLIST] 'Sets your wordlist file(required)'"
+       -w, --wordlist=[PATH_TO_WORDLIST] 'Sets your wordlist file(required)'
+       -a, --uagent=[USER_AGENT] 'Sets your desired User Agent in the request header (optional)'"
         )
        .get_matches();
     
+    let mut user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36";
     let target_host = args.value_of("url").unwrap();
     let wordlist = args.value_of("wordlist").unwrap();
+    
+    match args.occurrences_of("uagent") {
+        0 => user_agent = user_agent,
+        1 => user_agent = args.value_of("uagent").unwrap(),
+        _ => println!(
+            "Something went wrong witht the User-Agent.\nPlease try again."
+        ),
+    }
 
-    //Testing if we can read the host
+    //Testing if we can read the host & user agent
+    println!("------------------------------");
     println!("The url: {}",target_host);
+    println!("The User-Agent: {}",user_agent);
+    println!("------------------------------");
     
     // Opening the file
     let mut urls: Vec<String> = Vec::new(); 
@@ -31,10 +46,14 @@ fn main() {
        }
     }
 
+    // Making the request
     for path in urls {
+        let user_agent = format!("User-Agent: {}",user_agent);
         let target = format!("{}/{}", &target_host, &path);
-        //let target = url_encode(&target);
-        if let Err(err) = request(&target) {
+
+        //let response = isahc::head(&target).body(ua).send();
+
+        if let Err(err) = request(&target,&user_agent) {
             println!("Error with {}: {}",&target,err);
         }
         // Look up the "Ok(())"
@@ -42,17 +61,21 @@ fn main() {
     }
 }
 
-fn request (t: &str) -> Result<(), isahc::Error> {
-    // Send a GET request and wait for the response headers.
+fn request (t: &str,ua: &str ) -> Result<(), isahc::Error> {
+    // Send a HEAD request and wait for the response.
+    // We use the HEAD method because we don't need the body in this case
     // Must be `mut` so we can read the response body.
-    let response = isahc::get(t)?;
 
+    let response = Request::head(t).body(ua)?.send()?;
     // Handling the response by checking Status Code
     let status_response = response.status();
+
+
+    // Handling the Response Code
     if status_response.is_success(){
-        println!("[X]Success | {} | Status Code: {}",t,status_response);
+        println!("[{}] Success | {} | Status Code: {}",chrono::Local::now().format("%T"),t,status_response);
     } else if status_response.is_redirection() {
-        println!("[x]Redirect | {} | Status Code: {}",t,status_response);
+        println!("[{}] Redirect | {} | Status Code: {}",chrono::Local::now().format("%T"),t,status_response);
     }
 
     Ok(())
